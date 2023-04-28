7ebe6268784dc68a83f3c19746eacee70fadfeea
Jose Seto <joseseto8@gmail.com>
2023-04-27 20:19:37 -0700

    Added second fetch to pull information on the weather of the given city name

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,18 @@ function getWeather(){
     .then (function(response){
         response.json()
         .then (function(data){
-            console.log(data)
-            console.log("lat: " + data[0].lat)
-            console.log("lon: " + data[0].lon)
+            //console.log(data)
+            //console.log("lat: " + data[0].lat)
+            //console.log("lon: " + data[0].lon)
+            var lat = data[0].lat
+            var lon = data[0].lon
+            fetch("http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=c8af164d906f1649b6b1089b5ec881b1")
+            .then(function(report){
+                report.json()
+                .then(function(data){
+                    console.log(data)
+                })
+            })
         })
     })
 }
