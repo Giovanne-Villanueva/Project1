2f173f3017fca78417ee9940215ccb53dcac9103
Jose Seto <joseseto8@gmail.com>
2023-04-27 20:08:24 -0700

    Added geolocation fetch and parsed out latitude and longitude

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
-var cityName = "London"
-
+var cityName = "Placerville"
+//fetch("http://api.openweathermap.org/data/2.5/forecast?q="+ cityName + "&appid=c8af164d906f1649b6b1089b5ec881b1")
 function getWeather(){
-    var weatherAPI = fetch("http://api.openweathermap.org/data/2.5/forecast?q="+ cityName + "&appid=c8af164d906f1649b6b1089b5ec881b1")
-    .then (function(weatherAPI){
-        weatherAPI.json()
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=c8af164d906f1649b6b1089b5ec881b1")
+    .then (function(response){
+        response.json()
         .then (function(data){
             console.log(data)
+            console.log("lat: " + data[0].lat)
+            console.log("lon: " + data[0].lon)
         })
     })
 }
