ecb7b1e6fae0a24813a0973c166e76f828810555
Jose Seto <joseseto8@gmail.com>
2023-04-29 15:49:36 -0700

    Added a small click event to console out the data searched via api.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,40 @@
 var cityName = "Seattle"
+var searchCity = $("#search-city")
+var APIkey = "c8af164d906f1649b6b1089b5ec881b1"
+var searchButtonEl = $("#search-button")
 
-function getWeather(){
+function getWeather(city){
     //Calls geolocation API
-    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=c8af164d906f1649b6b1089b5ec881b1")
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + APIkey)
     .then (function(response){
         response.json()
         .then (function(data){
             var lat = data[0].lat
             var lon = data[0].lon
             //Calls 5 day 3 hour forecast with previous lat and lon
-            fetch("http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=c8af164d906f1649b6b1089b5ec881b1")
+            fetch("http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIkey)
             .then(function(report){
                 report.json()
                 .then(function(data){
                     //loops through data parsing one time frame per day
                     for(i = 7; i < data.list.length; i += 8){
-                        //converts unix timestamp to day of the week through day js
+                        //converts unix timestamp to day of the week through dayjs
                         var parseDays = dayjs.unix(data.list[i].dt).format('ddd')
-                        //console.log(parseDays)
+
                         if (parseDays == "Fri"){
-                            console.log("This is Friday:")
+                            console.log(dayjs.unix(data.list[i].dt).format('dddd, MMM D, YYYY'))
                             console.log(data.list[i])
                         }
                         else if(parseDays == "Sat"){
-                            console.log("This is Saturday:")
+                            console.log(dayjs.unix(data.list[i].dt).format('dddd, MMM D, YYYY'))
                             console.log(data.list[i])
                         }
                         else if(parseDays == "Sun"){
-                            console.log("This is Sunday:")
+                            console.log(dayjs.unix(data.list[i].dt).format('dddd, MMM D, YYYY'))
                             console.log(data.list[i])
                         }
                         else{
-                            console.log("trash data")
+                            return;
                         }
                     }
                 })
@@ -40,8 +43,12 @@ function getWeather(){
     })
 }
 
-function init(){
-    getWeather()
+function displayWeather(event){
+    event.preventDefault();
+    if(searchCity.val().trim()!==""){
+        city=searchCity.val().trim();
+        getWeather(city);
+    }
 }
 
-init();
+searchButtonEl.on("click", displayWeather)
